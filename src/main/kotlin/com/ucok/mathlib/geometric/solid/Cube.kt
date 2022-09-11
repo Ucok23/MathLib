fe8d232dafadf1c83366a02eafdd5c71a41e886b
ucok <ulubis98@gmail.com>
2022-09-11 19:32:56 +0700

    add fun to get face diagonal and space diagonal

--- src/main/kotlin/com/ucok/mathlib/geometric/solid/Cube.kt
+++ src/main/kotlin/com/ucok/mathlib/geometric/solid/Cube.kt
@@ -1,10 +1,14 @@
 package com.ucok.mathlib.geometric.solid
 
 import kotlin.math.pow
+import kotlin.math.sqrt
 
 class Cube(private val length: Double): Shape {
     override fun getArea(): Double = 6 * length
 
     override fun getVolume(): Double = length.pow(3)
 
+    fun getFaceDiagonal(): Double = sqrt(2 * length)
+
+    fun spaceDiagonal(): Double = sqrt(3 * length)
 }
