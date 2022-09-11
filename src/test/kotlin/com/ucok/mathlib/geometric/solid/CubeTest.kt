b3069406b7c779dee3023c5192482f9a18625444
ucok <ulubis98@gmail.com>
2022-09-11 19:47:19 +0700

    refactor CubeTest.kt

--- src/test/kotlin/com/ucok/mathlib/geometric/solid/CubeTest.kt
+++ src/test/kotlin/com/ucok/mathlib/geometric/solid/CubeTest.kt
@@ -2,23 +2,19 @@ package com.ucok.mathlib.geometric.solid
 
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
-
 internal class CubeTest {
 
-    @Test
-    fun getArea() {
-    }
+    private val testCube = Cube(2.0)
 
     @Test
-    fun getVolume() {
-    }
+    fun getArea() = assert(testCube.getArea() == 12.0)
 
     @Test
-    fun getFaceDiagonal() {
-    }
+    fun getVolume() = assert(testCube.getVolume() == 8.0)
 
     @Test
-    fun spaceDiagonal() {
-    }
+    fun getFaceDiagonal() = assert(testCube.getFaceDiagonal() == 2.0)
+
+    @Test
+    fun spaceDiagonal() = assert(String.format("%.2f", testCube.getSpaceDiagonal()).toDouble() == 2.45)
 }
