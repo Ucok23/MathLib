229069278d8cade7ef65754cea3ce1634f3d8bc4
ucok <ulubis98@gmail.com>
2022-09-12 05:41:54 +0700

    refactor

--- src/main/kotlin/com/ucok/mathlib/geometric/solid/Cube.kt
+++ src/main/kotlin/com/ucok/mathlib/geometric/solid/Cube.kt
@@ -3,12 +3,23 @@ package com.ucok.mathlib.geometric.solid
 import kotlin.math.pow
 import kotlin.math.sqrt
 
-class Cube(private val length: Double): Shape {
-    override fun getArea(): Double = 6 * length
+class Cube(var length: Double): Shape {
 
-    override fun getVolume(): Double = length.pow(3)
+    init {
+        when {
+            length < 1 -> throw IllegalArgumentException("length must be positive")
+        }
+    }
 
-    fun getFaceDiagonal(): Double = sqrt(2 * length)
+    override val area: Double
+        get() = 6 * length
 
-    fun getSpaceDiagonal(): Double = sqrt(3 * length)
+    override val volume: Double
+        get() = length.pow(3)
+
+    val faceDiagonal: Double
+        get() = sqrt(2 * length)
+
+    val spaceDiagonal: Double
+        get() = sqrt(3 * length)
 }
