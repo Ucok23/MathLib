c708f964107a64bc8a3b4f8a1c3e39ffd6ac0b88
ucok <ulubis98@gmail.com>
2022-09-11 19:27:52 +0700

    refactor

--- src/main/kotlin/com/ucok/mathlib/geometric/Bases.kt
+++ src/main/kotlin/com/ucok/mathlib/geometric/Bases.kt
@@ -1,6 +1,11 @@
 package com.ucok.mathlib.geometric
 
 data class Point(
-    val x: Number,
-    val y: Number
+    val x: Number?,
+    val y: Number?
+)
+
+data class Line(
+    val a: Point,
+    val b: Point
 )
