11a2e4284650f0b921f72249ef7582cbfdb17fc2
ucok <ulubis98@gmail.com>
2022-09-11 19:28:04 +0700

    refactor

--- src/main/kotlin/com/ucok/mathlib/geometric/solid/Cube.kt
+++ src/main/kotlin/com/ucok/mathlib/geometric/solid/Cube.kt
@@ -1,5 +1,10 @@
 package com.ucok.mathlib.geometric.solid
 
-class Cube {
+import kotlin.math.pow
+
+class Cube(private val length: Double): Shape {
+    override fun getArea(): Double = 6 * length
+
+    override fun getVolume(): Double = length.pow(3)
 
 }
