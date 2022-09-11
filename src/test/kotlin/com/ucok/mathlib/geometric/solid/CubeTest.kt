d0bb97e452f4c6c1c18be5bacbfa81161cdb5a29
ucok <ulubis98@gmail.com>
2022-09-12 06:16:44 +0700

    refactor

--- src/test/kotlin/com/ucok/mathlib/geometric/solid/CubeTest.kt
+++ src/test/kotlin/com/ucok/mathlib/geometric/solid/CubeTest.kt
@@ -16,8 +16,20 @@ internal class CubeTest {
     fun testVolume() = assert(testCube.volume == 8.0)
 
     @Test
+    fun testCalcVolume() = assert(Cube.calcVolume(2.0) == 8.0)
+
+    @Test
     fun testFaceDiagonal() = assert(testCube.faceDiagonal == 2.0)
 
     @Test
-    fun testSpaceDiagonal() = assert(String.format("%.2f", testCube.spaceDiagonal).toDouble() == 2.45)
+    fun testCalcFaceDiagonal() = assert(Cube.calcFaceDiagonal(2.0) == 2.0)
+
+    @Test
+    fun testSpaceDiagonal() =
+        assert(String.format("%.2f", testCube.spaceDiagonal).toDouble() == 2.45)
+
+    @Test
+    fun testCalcSpaceDiagonal() =
+        assert(String.format("%.2f",
+            Cube.calcSpaceDiagonal(2.0)).toDouble() == 2.45)
 }
