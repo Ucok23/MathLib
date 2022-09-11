5b3d1225e735e006aed2d278cbd28b0b97652945
ucok <ulubis98@gmail.com>
2022-09-11 19:33:15 +0700

    create CubeTest.kt

---
+++ src/test/kotlin/com/ucok/mathlib/geometric/solid/CubeTest.kt
@@ -0,0 +1,24 @@
+package com.ucok.mathlib.geometric.solid
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CubeTest {
+
+    @Test
+    fun getArea() {
+    }
+
+    @Test
+    fun getVolume() {
+    }
+
+    @Test
+    fun getFaceDiagonal() {
+    }
+
+    @Test
+    fun spaceDiagonal() {
+    }
+}
