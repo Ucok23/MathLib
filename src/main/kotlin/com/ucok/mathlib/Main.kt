d5208c034994c02a00f242e0bae1da1f5fdeac5f
ucok <ulubis98@gmail.com>
2022-09-12 05:55:01 +0700

    refactor

--- src/main/kotlin/com/ucok/mathlib/Main.kt
+++ src/main/kotlin/com/ucok/mathlib/Main.kt
@@ -3,6 +3,7 @@ package com.ucok.mathlib
 import com.ucok.mathlib.geometric.Line
 import com.ucok.mathlib.geometric.Point
 import com.ucok.mathlib.geometric.solid.Cube
+import com.ucok.mathlib.geometric.solid.RectPrism
 
 fun main() {
     val point = Point(x = null, y = null)
@@ -12,4 +13,9 @@ fun main() {
     val cube = Cube(12.0)
     cube.length = 1.0
     println(cube.area)
+
+    val rectPrism = RectPrism(2.0, 2.0, 1.0)
+    println(rectPrism.area)
+    println(rectPrism.volume)
+    println(rectPrism.spaceDiagonal)
 }
