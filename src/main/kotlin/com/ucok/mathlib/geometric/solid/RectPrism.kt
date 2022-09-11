ee0adc077182e248d0eac9d58a39f3363a9a1b7b
ucok <ulubis98@gmail.com>
2022-09-12 06:38:18 +0700

    refactor

--- src/main/kotlin/com/ucok/mathlib/geometric/solid/RectPrism.kt
+++ src/main/kotlin/com/ucok/mathlib/geometric/solid/RectPrism.kt
@@ -5,11 +5,20 @@ import kotlin.math.sqrt
 
 class RectPrism(var p: Double, var l: Double, var t:Double): Shape {
     override val area: Double
-        get() = 2 * (p * l + p * t + l * t)
+        get() = calcArea(p, l, t)
 
     override val volume: Double
-        get() = p * l * t
+        get() = calcVolume(p, l, t)
 
     val spaceDiagonal: Double
-        get() = sqrt(p.pow(2) + l.pow(2) + t.pow(2))
+        get() = calcSpaceDiagonal(p, l, t)
+
+    companion object {
+        fun calcArea(p: Double, l: Double, t: Double) = 2 * (p * l + p * t + l * t)
+
+        fun calcVolume(p: Double, l: Double, t: Double) = p * l * t
+
+        fun calcSpaceDiagonal(p: Double, l: Double, t: Double) =
+            sqrt(p.pow(2) + l.pow(2) + t.pow(2))
+    }
 }
